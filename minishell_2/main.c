99a9197be891f3eb24565dc0bca4e5b3789a66ab
Sezarfen <110563468+sezarfen@users.noreply.github.com>
2023-09-12 15:53:33 +0000

    Update main.c

--- minishell_2/main.c
+++ minishell_2/main.c
@@ -87,6 +87,8 @@ int main(int ac, char **av, char **the_env)
 			env_list = export(lexer->tokens[1], env_list);
 		if (!ft_strncmp(lexer->tokens[0], "env", ft_strlen(lexer->tokens[0])))
 			print_env(env_list);
+		if (!ft_strncmp(lexer->tokens[0], "unset", ft_strlen(lexer->tokens[0])))
+			unset(lexer->tokens[1], env_list);
 	}
 	return (0);
 }
