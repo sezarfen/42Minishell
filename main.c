7f733d5b509664f70420e47d24fe2a25c1dc285c
Sezarfen <110563468+sezarfen@users.noreply.github.com>
2023-09-19 11:39:49 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,14 +4,21 @@ t_lexer	*init_lexer(void)
 {
 	t_lexer	*tlexer;
 	char	*str;
+	char	*prompt;
+	char 	cwd[256];
 
-	str = readline("minishell$ ");
+	getcwd(cwd, 256);
+	prompt = set_propmt(cwd);
+	str = readline(prompt);
+	add_history(str);
 	tlexer = malloc(sizeof(t_lexer));
-	tlexer->tokens = the_lexer(str);
+	tlexer->tokens = the_lexer(str, 0, 0, 0);
 	tlexer->size = split_len(tlexer->tokens);
 	free(str);
+	free(prompt);
 	return (tlexer);
 }
+
 
 t_env	*set_env(char **the_env)
 {
@@ -33,24 +40,106 @@ t_env	*set_env(char **the_env)
 	return (env);
 }
 
+t_env_exp	*set_env_exp(char **the_env)
+{
+	t_env_exp	*env_exp;
+
+	env_exp = malloc(sizeof(t_env_exp));
+	env_exp->env = set_env(the_env);
+	env_exp->exp = set_env(the_env);
+	return (env_exp);
+}
+
 int main(int ac, char **av, char **the_env)
 {
 	t_lexer		*lexer;
 	t_parser	*parser;
-	t_env		*env;
+	t_env_exp	*env_exp;
 
-	env = set_env(the_env);
-	lexer = init_lexer();
-	int	i = 0;
-	while (lexer->tokens[i])
+	env_exp = set_env_exp(the_env);
+	while (1)
 	{
-		lexer->tokens[i] = cleaner(lexer->tokens[i], env);
-		i++;
+		lexer = init_lexer();
+		if (lexer->tokens[0] == NULL || lexer_control(lexer))
+			continue ;
+		parser = set_parser(lexer, 0, 0);
+		fill_parser(parser, env_exp->env);
+		to_execute(parser, the_env, env_exp->env);
 	}
-	i = 0;
-	while (lexer->tokens[i])
-		printf("%s\n", lexer->tokens[i++]);
-	//printf("\n----------PARSER----------\n");
-	//parser = set_parser(lexer);
 	return (0);
 }
+
+/*
+		LEXER İÇERİKLERİNİ CLEANER ' A SOKMA [DAHA SONRA YAPILMASI DAHA DOĞRU OLABİLİR]
+		while (lexer->tokens[i])
+		{
+			lexer->tokens[i] = cleaner(lexer->tokens[i], env);
+			i++;
+		}
+
+		// PARSER İÇERİKLERİNİ EKRANA YAZDIRMA
+		i = 0;
+		t_parser *temp = parser;
+		while (temp)
+		{
+			while (temp->cmds[i])
+				printf("(%s) ", temp->cmds[i++]);
+			i = 0;
+			temp = temp->next;
+			if (temp)
+				printf("\n---pipe---\n");
+		}
+
+		// PRINT THE CLEANED PARSER
+		printf("\n=========CLEANED PARSER=========\n");
+		t_parser	*temp;
+		int			i;
+		temp = parser;
+		i = 0;
+		while (temp)
+		{
+			while (temp->cmds[i])
+			{
+				temp->cmds[i] = cleaner(temp->cmds[i], env_list);
+				printf("%s ", temp->cmds[i++]);
+			}
+			i = 0;
+			printf("\n");
+			temp = temp->next;
+			if (temp)
+				printf("\n|==(pipe found)==|\n");
+		}
+
+		// DIFFERENT PARSER PRINT (WITH ATTRIBUTES)
+
+		int i = 0;
+		t_parser *temp = parser;
+		while (temp)
+		{
+			while (temp->cmds[i])
+				printf("(%s) ", temp->cmds[i++]);
+			printf("\n");
+			printf("fd_in:%d\nfd_out:%d\nis_builtin:%d\nhd_in:%d\n", temp->fd_in , temp->fd_out, temp->is_builtin, temp->hd_in);
+			i = 0;
+			temp = temp->next;
+			if (temp)
+				printf("\n---pipe---\n");
+		}
+
+/////////////////////////////////////////
+	/*
+		int i = 0;
+		t_parser *temp = parser;
+		while (temp)
+		{
+			while (temp->cmds[i])
+				printf("(%s) ", temp->cmds[i++]);
+			printf("\n");
+			printf("fd_in:%d\nfd_out:%d\nis_builtin:%d\nhd_in:%d\n", temp->fd_in , temp->fd_out, temp->is_builtin, temp->hd_in);
+			i = 0;
+			temp = temp->next;
+			if (temp)
+				printf("\n---pipe---\n");
+		}
+	*/
+//////////////////////////////////////////
